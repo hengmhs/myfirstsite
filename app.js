6677e2a5ebebed202b66a08fc68c3dee6ba07570
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-06-13 20:07:14 +0800

    Refactor with latest Express

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
+
+const path = require('path');
 const port = 5000
 
 // serve files from the views folder
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
 app.get('/about', (req, res) => {
-  res.render('about')
-})
+  res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/contact-me.html'));
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'views/404.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
